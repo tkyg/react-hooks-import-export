8ad6735ce4d1a18839789c0ba38d24f0b21dc0b9
tkyg <80114433+tkyg@users.noreply.github.com>
2022-05-12 17:55:14 -0400

    complete

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,21 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import * as RMFunctions from "./parks/RockyMountain"
+console.log(RMFunctions.trees)
+RMFunctions.wildlife()
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+  return(
+    <>
+    <div>
+      <MesaVerde />
+      </div>
+    <h1>Colorado State Parks!</h1>;
+    </>
+    ) 
 }
+
+export default ColoradoStateParks
